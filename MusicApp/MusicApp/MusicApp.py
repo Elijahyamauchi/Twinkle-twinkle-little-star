962a877753893ef876d0071a529f1bd36c0d6afa
Elijahyamauchi <60528081+Elijahyamauchi@users.noreply.github.com>
2023-12-18 14:01:44 -1000

    Update MusicApp.py
    Made it so that the keyboard numbers activate the tone

--- MusicApp/MusicApp/MusicApp.py
+++ MusicApp/MusicApp/MusicApp.py
@@ -3,15 +3,13 @@
 #step 1) make a way to play each note
 #setp 2) 
 
-#from re import A
 import numpy as np
 import sounddevice as sd
+import keyboard
+import time
 
-import numpy as np
-import sounddevice as sd
-
-def generate_frequency(frequency, duration, samplerate=44100, amplitude=0.3):
-
+def generate_frequency(frequency,duration=1.0, samplerate=44100, amplitude=0.3):
+    
     # Generate the audio signal for the note
     t = np.linspace(0, duration, int(duration * samplerate), endpoint=False)
     signal = amplitude * np.sin(2 * np.pi * frequency * t)
@@ -21,19 +19,34 @@ def generate_frequency(frequency, duration, samplerate=44100, amplitude=0.3):
 def play(note):
     sd.play(note, samplerate=44100)
     sd.wait()
-
+def play_note(key,frequency):
+    if key in key_mapping:
+        play(frequency)
+        
 # Define note
-A= generate_frequency(220, duration=1)
-B= generate_frequency(246.94, duration=1)
-C= generate_frequency(261.63, duration=1)
-D= generate_frequency(293.66, duration=1)
-E= generate_frequency(329.63, duration=1)
-F= generate_frequency(349.23, duration=1)
-G= generate_frequency(392, duration=1)
+A= generate_frequency(220)
+B= generate_frequency(246.94)
+C= generate_frequency(261.63)
+D= generate_frequency(293.66)
+E= generate_frequency(329.63)
+F= generate_frequency(349.23)
+G= generate_frequency(392)
+
+key_mapping={
+    '1':A,
+    '2':B,
+    '3':C,
+    '4':D,
+    '5':E,
+    '6':F,
+    '7':G,
+    }
 
 
-play(G)
-play(A)
+for key,frequency in key_mapping.items():
+    keyboard.on_press_key(key, lambda event, k=key,freq=frequency: play_note(k,freq))
+keyboard.wait('esc')
+
 
 
 
